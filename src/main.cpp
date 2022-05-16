769f24ea247c82a93128d3ca5b85255a26f7c4ef
WizMe-M <timkin.moxim@mail.ru>
2022-05-16 23:16:14 +0300

    read data from display and switch lighting by it

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define RX_PIN 0
+#define TX_PIN 1
+
+const String ON = "ON";
+const String OFF = "OFF";
+
+String received_data;
+SoftwareSerial displaySerial(TX_PIN, RX_PIN);
+
 void setup() {
-// write your initialization code here
+    displaySerial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    //светодиод инвертирован
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-// write your code here
+    while (displaySerial.available()) {
+        char c = displaySerial.read();
+        received_data += c;
+
+        // проверяем на совпадение полученной строки с константами и обнуляем при совпадении
+        if (received_data == ON) {
+            digitalWrite(LED_BUILTIN, LOW);
+            received_data = "";
+        } else if (received_data == OFF) {
+            digitalWrite(LED_BUILTIN, HIGH);
+            received_data = "";
+        }
+    }
 }
